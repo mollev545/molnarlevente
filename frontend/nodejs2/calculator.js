9591f78129d44bdc0c7167cdd837fbc23c41ba89
mollev545 <mollev545@hengersor.hu>
2024-12-06 11:00:09 +0100

    Szerver létrehozása
    Videó alapján

---
+++ frontend/nodejs2/calculator.js
@@ -0,0 +1,6 @@
+//function(__dirname, __filename){
+function add(num1,num2){
+return num1 + num2;
+}
+module.exports=add;
+//}
--- frontend/nodejs2/index.js
+++ frontend/nodejs2/index.js
@@ -1,10 +1,22 @@
-//wrapper function
-//function(__dirname, __filename, module, require, exports){
-
-
-console.log("__dirname");
-console.log("__filename");
-
-
-
-//}
+//wrapper function      //relative, absolute útvonal fogalom
+//function (__dirname, __filename) {
+ 
+console.log(__dirname);
+console.log(__filename);
+ 
+const add = require ('./calculator');
+ 
+console.log(add(2, 3));
+ 
+const http = require('http');
+ 
+const server = http.createServer(function (req, res)
+{
+    res.writeHead(200)
+    res.end('Hello, World!')
+});
+ 
+server.listen(8080)
+ 
+// }
+ 
