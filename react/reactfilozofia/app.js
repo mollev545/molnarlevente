532372b29a4e70784758013cbaccbba96eedcd0c
mollev545 <mollev545@hengersor.hu>
2024-12-09 12:24:24 +0100

    12.09

---
+++ react/reactfilozofia/app.js
@@ -0,0 +1,25 @@
+const { create } = require("domain");
+
+const btn = document.createElement('button');
+
+btn.onclick = function()
+{
+alert("Ez egy natív kód");    
+}
+btn.innerHTML= "Natív gomb";
+
+document.getElementById("nativ-button-container")
+
+appendChild(btn);
+
+const btn = createElement("button")
+{
+onClick: function()
+{
+    alert("Ez egy natív kód");
+}
+
+}
+"React gomb"
+ReactDOM.render(gomb,document.getElementById("react-button container"));
+
