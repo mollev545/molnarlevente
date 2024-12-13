22f471047271e23c5c9cf78befa0489119008687
mollev545 <mollev545@hengersor.hu>
2024-12-13 13:22:22 +0100

    module1

--- react/reactfilozofia/app.js
+++ react/reactfilozofia/app.js
@@ -1,5 +1,5 @@
 const { create } = require("domain");
-
+//natív --> hogyan?
 const btn = document.createElement('button');
 
 btn.onclick = function()
@@ -8,21 +8,24 @@ alert("Ez egy natív kód");
 }
 btn.innerHTML= "Natív gomb";
 
-document.getElementById("nativ-button-container").appendChild(btn);
+document.getElementById("nativ-button-container").appendChild(btn); //btn változóba létrehozunk egy gombot
 
 appendChild(btn);
-
+//react --> mit akarok látni?
 React.createElement("button",
 
 {
 onClick: function()
 {
-    alert("Klikk esemény történt");
+    alert("Klikk esemény történt"); //gomb funkciója
 },
 
 },
 "React gomb"
 );
+
+//első paraméter az elemet amit használni szeretnénk,
+//második paramitér egy referencia egy natív DOM elemre,hivatkozáspont megadása
 ReactDOM.render(gomb,document.getElementById("react-button container"));
 
 ReactDOM.render(
